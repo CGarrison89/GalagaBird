d5f22d711639f57ecafe95f3e790327dddbb2b68
Aaron <abatilo@uccs.edu>
2015-05-04 13:49:37 -0600

    Game stops when a bound is hit

--- Assets/Scripts/FloorCollision.cs
+++ Assets/Scripts/FloorCollision.cs
@@ -6,6 +6,7 @@ public class FloorCollision : MonoBehaviour {
 	void OnTriggerEnter2D (Collider2D collider) {
 		if (collider.gameObject.CompareTag("Player")) {
 			Debug.Log ("Dead");
+			Time.timeScale = 0;
 		}
 	}
 }
