d850302e082db4ffe46e014dc36d6eed7c91d176
Aaron <abatilo@uccs.edu>
2015-05-04 13:45:51 -0600

    Background now recycles and there's collision detection for above and below the map

---
+++ Assets/BackgroundMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMovement : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		if (collider.gameObject.CompareTag ("Background")) {
+			Vector3 currentPosition = collider.gameObject.transform.position;
+			currentPosition += new Vector3(15.98f, 0, 0);
+			collider.gameObject.transform.position = currentPosition;
+			Debug.Log ("Reset");
+		}
+	}
+}
---
+++ Assets/BoundsMovement.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoundsMovement : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		Vector3 position = player.transform.position;
+		transform.position = new Vector3 (position.x - 3f, 0f, -2f);
+	}
+}
