05497ae3352ef7a4cfc7e682ee0a9e899563b4c5
Aaron <abatilo@uccs.edu>
2015-05-04 13:09:48 -0600

    Camera follows bird

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public GameObject Player;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (Player.transform.position.x - 2.5f, 0f, -10f);
+	}
+}
