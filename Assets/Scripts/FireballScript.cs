2bcc2b38db0ad3857f5b8d6388edb1ce9db0fe35
JaxFirehart <Kylecondo@gmail.com>
2015-05-04 16:39:08 -0600

    Implemented DrT move and attack but not spawning

---
+++ Assets/Scripts/FireballScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(-.1f, 0, 0);
+    }
+
+    public void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
