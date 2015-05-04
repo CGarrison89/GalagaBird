29ac49e51ff35bdf422ddd5dd5b08639cc65c973
Aaron <abatilo@uccs.edu>
2015-05-04 12:59:43 -0600

    Bird flaps up and down

---
+++ Assets/FloorCollision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorCollision : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		Debug.Log ("Dead");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/BirdMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdMovement : MonoBehaviour {
+
+	public Vector3 Gravity;
+	public Vector3 UpwardsForce;
+	Vector3 velocityForce = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Update() {
+		//Left button is clicked
+		if (Input.GetMouseButtonDown (0)) {
+			velocityForce += UpwardsForce * Time.deltaTime;
+
+			if (velocityForce.y > 4) {
+				velocityForce.y = 4;
+			}
+
+			transform.position += velocityForce * Time.deltaTime;
+		}
+	}
+
+	void FixedUpdate () {
+		//Handle gravity positioning
+		velocityForce += Gravity * Time.deltaTime;
+		transform.position += velocityForce * Time.deltaTime;
+	}
+}
