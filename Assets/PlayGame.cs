61f06bca7427955c9dea738244df4fa6e5d58c8d
Aaron <abatilo@uccs.edu>
2015-05-04 20:26:59 -0600

    Integrated Tyler's menu

---
+++ Assets/PlayGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayGame : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Application.LoadLevel("scene_0");
+		}
+	}
+}
